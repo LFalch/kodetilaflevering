b8a24093763934794b2778d2f7467f85e37917a1
Carditis <45385019+Carditis@users.noreply.github.com>
2019-10-02 00:32:26 +0200

    fuck this shit
    eji tgnhirjnhbth uvjhifk

---
+++ src/kodetilaflevering/NextSquare.java
@@ -0,0 +1,20 @@
+package kodetilaflevering;
+
+public class NextSquare {
+    public static void main(String[] args) {
+        System.out.print(nextSquare(-8));
+    }
+    
+    public static int nextSquare(int n) {
+        //Her laver vi en casting. Det er gjort fordi kvadrartrodden normalt retunere en double
+        int h = (int)Math.sqrt(n);
+        int g = h+1;
+        return(g*g);
+
+        
+    }
+    
+    
+    
+
+}
