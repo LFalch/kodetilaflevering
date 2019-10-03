9d24415606b496e764777cc77ff133742f2a8d40
Carditis <45385019+Carditis@users.noreply.github.com>
2019-10-03 14:02:53 +0200

    UpdTED

--- src/kodetilaflevering/NextSquare.java
+++ src/kodetilaflevering/NextSquare.java
@@ -2,10 +2,15 @@ package kodetilaflevering;
 
 public class NextSquare {
     public static void main(String[] args) {
-        System.out.print(nextSquare(-8));
+        System.out.print(nextSquare(-16));
     }
     
     public static int nextSquare(int n) {
+    	if (n < 0) {
+    		int h = (int)Math.sqrt(n*-1);
+    		int g = h+1;
+    		return(g*g);
+    	}
         //Her laver vi en casting. Det er gjort fordi kvadrartrodden normalt retunere en double
         int h = (int)Math.sqrt(n);
         int g = h+1;
