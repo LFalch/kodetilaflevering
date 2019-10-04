fd7ad303a719ff2c8c728700b3ba9dfdc6c52a23
Carditis <45385019+Carditis@users.noreply.github.com>
2019-10-04 22:18:28 +0200

    no

--- src/kodetilaflevering/NextPrime.java
+++ src/kodetilaflevering/NextPrime.java
@@ -1,3 +1,5 @@
+package kodetilaflevering;
+
 
 public class NextPrime {
 
---
+++ src/kodetilaflevering/Telefon.java
@@ -0,0 +1,34 @@
+package kodetilaflevering;
+
+import java.util.Scanner;
+
+public class Telefon {
+	public static void main(String[] args) {
+		Scanner consol = new Scanner(System.in);
+		String n = consol.next();
+		int k = n.length();
+		int i;
+		for (i = 0; i < k; i++) {
+			char c = n.charAt(i);
+			//if(c = int) {
+				
+			//}
+			System.out.println(c);
+		}
+		
+		phoneTestet(n);
+		
+		
+	}
+
+	public static String phoneTestet(String n) {
+		
+		int length = n.length();
+		System.out.print(length);
+		
+		
+		
+		String h = "Hej";
+		return(h);
+	}
+}
