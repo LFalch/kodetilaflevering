f3550170fcc3951a043f1fc2a18320f6328075ce
Tchaxed <Christianukesting@yahoo.dk>
2019-10-07 12:43:51 +0200

    Testet et fix

--- src/kodetilaflevering/Password.java
+++ src/kodetilaflevering/Password.java
@@ -13,6 +13,7 @@ public class Password {
 		int småBogstav = 0;
 		int storeBogstav = 0;
 		int count = 0;
+		int ikkeAlfa = 0;
 		char ch;
 
 		if (length >= 7)
@@ -20,17 +21,17 @@ public class Password {
 			while (count < length)
 			{
 				ch = s.charAt(count);
-				if (Character.isDigit(ch))
-				{
+				if (Character.isDigit(ch)) {
 					tal = tal + 1;
 				}
-				if (Character.isLowerCase(ch))
-				{
+				if (Character.isLowerCase(ch)) {
 					småBogstav = småBogstav + 1;
 				}
-				if (Character.isUpperCase(ch))
-				{
+				if (Character.isUpperCase(ch)) {				
 					storeBogstav = storeBogstav + 1;
+				}
+				if (s.matches("^[a-zA-Z0-9]+$")) {
+					ikkeAlfa = ikkeAlfa + 1;
 				}
 				count = count + 1;
 			}
@@ -38,21 +39,19 @@ public class Password {
 		if (length < 7) {
 			System.out.println("unacceptable");
 		} else {
-			if (length == 7 && småBogstav >=1 && storeBogstav >= 1) {
+			if (length == 7 && småBogstav >=1 && storeBogstav >= 1 && ikkeAlfa >= 1) {
 				System.out.println("acceptable");
 			} else { 
-				if (length == 8 && småBogstav >=1 && storeBogstav >= 1) {
-				System.out.println("acceptable");
-			} else {
-				if (length >=9 && småBogstav >=1 && storeBogstav >= 1) {
-					System.out.println("strong");
+				if (length == 8 && småBogstav >=1 && storeBogstav >= 1 && ikkeAlfa >= 1) {
+					System.out.println("acceptable");
+				} else {
+					if (length >=9 && småBogstav >=1 && storeBogstav >= 1&& ikkeAlfa >= 1) {
+						System.out.println("strong");
+					}
 				}
-			}
 			}
 		}
 	}
 }
-//Der skal fikses så der kun kommer 1 svar ud!! FIKSET
-//Der kommer ikke svar ud fra Strong... Kun acceptable
 //Der skal fikses så der tages højde for ikke alfanumerisk
 //Fix input ved kode som: ABCD!#$&
